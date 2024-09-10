bd330b5900c49e11b38ab0eb494bf297f554ed51
Hexegol <178416097+Hexegol@users.noreply.github.com>
2024-09-10 18:00:25 +0200

    Update Cheat.h

--- Cheat.h
+++ Cheat.h
@@ -5,19 +5,21 @@
 class Cheat
 {
 private:
-	float speed;
 	std::wstring processName;
-	DWORD GetPID(const char* ProcessName);
 	//MODULEENTRY32 GetModule(const char* moduleName, unsigned long ProcessID);
+	DWORD GetPID(const char* ProcessName);
 	uintptr_t dwGetModuleBaseAddress(TCHAR* lpszModuleName, DWORD pID);
 public:
 	Cheat();
 	Cheat(std::wstring processName);
-
+	
 	void ChangeSpeed(bool resetvalues);
 	void ChangeHitbox(bool resetvalues);
 	void ChangeReach(bool resetvalues);
 	void EnableFly(bool resetvalues);
+	void EnableAimbot(bool resetvalues);
+	void EnableAirJump(bool resetvalues);
+	void EnableNoKnockback(bool resetvalues);
 	void selfDestruct();
 	void emptyRecycleBin();
 };
